139708295ebcd2da7e6676e797139bf291935762
Bao Long <baolong9393@gmail.com>
2016-03-12 20:28:33 +0000

    Created main()
    Created main() to start the app. Create fucntions to handle different
    task.

--- readLogs.py
+++ readLogs.py
@@ -24,33 +24,38 @@ def identifyEmitterAndDistance (signalMessage, receiveTime):
 def drawCircleWithCentrePoint (x, y, r, canvas, outlineColor):
 	return canvas.create_oval(x - r, y - r, x + r, y + r, outline = outlineColor)
 
-master = Tk()
+# Read Log file
+def readLogFile(url, fileName):
+	os.system("cd " + url)# url = "/Users/baolongngo/Documents/UoP/FinalYear/Project"
+	f = open(fileName,"r")# filename = "logs.txt"
+	readf = f.readlines()
+	for line in readf:
+		distance = 0
+		times = line.split()
+		emitterCodeNdistance = identifyEmitterAndDistance(times[0], times[1])
+		print emitterCodeNdistance[0]
+		print emitterCodeNdistance[1]
+	#close file
+	if f.closed == "False":
+	    f.close()
 
-w = Canvas(master, width=200, height=200)
-w.pack()
+#main
+def main():
+	master = Tk()
+	w = Canvas(master, width=600, height=600)
+	w.pack()
 
-# w.create_line(0, 0, 200, 100)
-# w.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
+	# w.create_line(0, 0, 200, 100)
+	# w.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
 
-# w.create_rectangle(50, 25, 150, 75, fill="blue")
-# w.create_oval(10, 50, 50, 80, outline="green")
-# drawCircleWithCentrePoint(0, 0, 50, w)
-drawCircleWithCentrePoint(200, 0, 150, w, "red")
-drawCircleWithCentrePoint(0, 200, 132, w, "green")
-drawCircleWithCentrePoint(0, 0, 150, w, "blue")
+	w.create_rectangle(200, 200, 400, 400, outline="black")
+	# w.create_oval(10, 50, 50, 80, outline="green")
+	# drawCircleWithCentrePoint(0, 0, 50, w)
+	drawCircleWithCentrePoint(400, 200, 150, w, "red")
+	drawCircleWithCentrePoint(200, 400, 132, w, "green")
+	drawCircleWithCentrePoint(200, 200, 150, w, "blue")
+	mainloop()
 
-mainloop()
+main()
 
-# os.system("cd /Users/baolongngo/Documents/UoP/FinalYear/Project")
-# f = open("logs.txt","r")
-# readf = f.readlines()
-# for line in readf:
-# 	distance = 0
-# 	times = line.split()
-# 	emitterCodeNdistance = identifyEmitterAndDistance(times[0], times[1])
-# 	print emitterCodeNdistance[0]
-# 	print emitterCodeNdistance[1]
-# #close file
-# if f.closed == "False":
-#     f.close()
 
